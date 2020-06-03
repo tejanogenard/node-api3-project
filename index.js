7ae0df225ad22acd2e6778a91dc4a537c09601a5
Genard Ivan Tejano <genardivanteajno@Genards-MBP.socal.rr.com>
2020-06-03 13:35:43 -0700

    index.js, server is built and now running

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require("./server.js")
+
+const port = 6000
+
+server.listen(port, () => {
+    console.log(`\n== Server is Running on http://localhost:${port} ==\n`)
+})
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const post = require("./postDb.js")
 const router = express.Router();
 
 router.get('/', (req, res) => {
